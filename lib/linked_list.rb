82e7fc6bf7b8942137b47440b0fea5dc7b1d39f7
Nicholas <69174708+berubenic@users.noreply.github.com>
2020-10-12 17:30:55 -0400

    seperated both classes in two files

---
+++ lib/linked_list.rb
@@ -0,0 +1,135 @@
+# frozen_string_literal: true
+
+# LinkedList
+class LinkedList
+  attr_reader :head, :tail
+  def initialize
+    @head = nil
+    @tail = nil
+  end
+
+  def append(value)
+    this_node = Node.new(value)
+    if @head.nil?
+      @head = @tail = this_node
+    else
+      @tail.next_node = this_node
+      @tail = this_node
+    end
+  end
+
+  def prepend(value)
+    this_node = Node.new(value)
+    if @head.nil?
+      @head = @tail = this_node
+    else
+      this_node.next_node = @head
+      @head = this_node
+    end
+  end
+
+  def size
+    count = 0
+    current = @head
+    until current.nil?
+      current = current.next_node
+      count += 1
+    end
+    count
+  end
+
+  def at(index)
+    return @head if index.zero?
+
+    return unless index.positive?
+
+    current = @head
+    index.times do
+      current = current.next_node
+    end
+    current
+  end
+
+  def pop
+    current = @head
+    previous = nil
+    until current.next_node.nil?
+      previous = current
+      current = current.next_node
+    end
+    @tails = previous
+    @tails.next_node = nil
+  end
+
+  def contains?(value)
+    current = @head
+    until current.nil?
+      return true if current.value == value
+
+      current = current.next_node
+    end
+    false
+  end
+
+  def find(value)
+    current = @head
+    index = 0
+    until current.nil?
+      return index if current.value == value
+
+      current = current.next_node
+      index += 1
+    end
+  end
+
+  def to_s
+    current = @head
+    until current.nil?
+      print "(#{current.value}) -> "
+      current = current.next_node
+    end
+    print "nil \n"
+  end
+
+  def insert_at(value, index)
+    if @head.nil?
+      this_node = Node.new(value)
+      @head = this_node
+    end
+    if index.zero?
+      this_node = Node.new(value, @head)
+      @head = this_node
+    end
+    return unless index.positive?
+
+    current = @head
+    before_current = @head
+
+    (index - 1).times do
+      before_current = before_current.next_node
+    end
+    index.times do
+      current = current.next_node
+    end
+    this_node = Node.new(value)
+    after_current = before_current.next_node
+    before_current.next_node = this_node
+    this_node.next_node = after_current
+  end
+
+  def remove_at(index)
+    current = @head
+    before_current = @head
+    @head = current.next_node if index.zero?
+    return unless index.positive?
+
+    (index - 1).times do
+      before_current = before_current.next_node
+    end
+    index.times do
+      current = current.next_node
+    end
+    before_current.next_node = current.next_node
+    current.value = nil
+  end
+end
--- main.rb
+++ main.rb
@@ -1,145 +1,35 @@
 # frozen_string_literal: true
 
-# LinkedList
-class LinkedList
-  attr_reader :head, :tail
-  def initialize
-    @head = nil
-    @tail = nil
-  end
+require_relative 'lib/linked_list'
+require_relative 'lib/node'
 
-  def append(value)
-    this_node = Node.new(value)
-    if @head.nil?
-      @head = @tail = this_node
-    else
-      @tail.next_node = this_node
-      @tail = this_node
-    end
-  end
+list = LinkedList.new
 
-  def prepend(value)
-    this_node = Node.new(value)
-    if @head.nil?
-      @head = @tail = this_node
-    else
-      this_node.next_node = @head
-      @head = this_node
-    end
-  end
+list.append(20)
+list.append(21)
+list.append(22)
+list.append(23)
 
-  def size
-    count = 0
-    current = @head
-    until current.nil?
-      current = current.next_node
-      count += 1
-    end
-    count
-  end
+list.prepend(1)
+list.prepend(2)
+list.prepend(3)
+list.prepend(4)
 
-  def at(index)
-    return @head if index.zero?
+list.to_s
 
-    return unless index.positive?
+p list.size
 
-    current = @head
-    index.times do
-      current = current.next_node
-    end
-    current
-  end
+p list.at(2).value
 
-  def pop
-    current = @head
-    previous = nil
-    until current.next_node.nil?
-      previous = current
-      current = current.next_node
-    end
-    @tails = previous
-    @tails.next_node = nil
-  end
+list.pop
+list.to_s
 
-  def contains?(value)
-    current = @head
-    until current.nil?
-      return true if current.value == value
+p list.contains?(21)
 
-      current = current.next_node
-    end
-    false
-  end
+p list.find(2)
 
-  def find(value)
-    current = @head
-    index = 0
-    until current.nil?
-      return index if current.value == value
+list.insert_at(50, 4)
+list.to_s
 
-      current = current.next_node
-      index += 1
-    end
-  end
-
-  def to_s
-    current = @head
-    until current.nil?
-      print "(#{current.value}) -> "
-      current = current.next_node
-    end
-    print "nil \n"
-  end
-
-  def insert_at(value, index)
-    if @head.nil?
-      this_node = Node.new(value)
-      @head = this_node
-    end
-    if index.zero?
-      this_node = Node.new(value, @head)
-      @head = this_node
-    end
-    return unless index.positive?
-
-    current = @head
-    before_current = @head
-
-    (index - 1).times do
-      before_current = current.next_node
-    end
-    index.times do
-      current = current.next_node
-    end
-    this_node = Node.new(value)
-    after_current = before_current.next_node
-    before_current.next_node = this_node
-    this_node.next_node = after_current
-  end
-
-  def remove_at(index)
-    current = @head
-    before_current = @head
-    @head = current.next_node if index.zero?
-    return unless index.positive?
-
-    (index - 1).times do
-      before_current = current.next_node
-    end
-    index.times do
-      current = current.next_node
-    end
-    before_current.next_node = current.next_node
-    current.value = nil
-  end
-end
-
-# Node
-class Node
-  attr_accessor :value, :next_node
-
-  def initialize(value, next_node = nil)
-    @value = value
-    @next_node = next_node
-  end
-end
+list.remove_at(4)
+list.to_s
