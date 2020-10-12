0c457097e31b2597cfae779702ab2e34591b9d0d
Nicholas <69174708+berubenic@users.noreply.github.com>
2020-10-12 13:42:33 -0400

    append method in LinkedList

--- main.rb
+++ main.rb
@@ -1,15 +1,41 @@
 # frozen_string_literal: true
 
 class LinkedList
-  def append(value); end
+  # setup head ,remmember we traverse a linkedlist from the head
+  def initialize
+    @head = nil
+    @tail = nil
+  end
+
+  def append(value)
+    # create a new node
+    this_node = Node.new(value)
+    if @head.nil?
+      @head = this_node
+      return
+    end
+
+    current = @head
+    # until current.nil means until we reach the last node
+    current = current.next_node until current.next_node.nil?
+
+    # point current(last node) to our new node
+    current.next_node = this_node
+  end
 end
 
 class Node
-  attr_accessor :next_node
-  attr_reader :value
+  attr_accessor :value, :next_node
 
-  def initialize(value = nil)
-    @next_node = nil
+  def initialize(value, next_node = nil)
     @value = value
+    @next_node = next_node
   end
 end
+
+list = LinkedList.new
+
+p list
+
+p list.append(10)
+p list.append(20)
